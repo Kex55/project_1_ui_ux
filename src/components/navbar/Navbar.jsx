b99efe16f496ee548905c6edc6400c8d899b1f48
viktorherman55 <viktorherman55@gmail.com>
2024-04-22 17:48:29 +0200

    Navbar menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import './navbar.css';
 import logo from '../../assets/logo.svg';
 
+const Menu = () => (
+  <>
+  <p><a href="#home">Home</a></p>
+  <p><a href="#wgpt4">What is GPT?</a></p>
+  <p><a href="#possibility">Open AI</a></p>
+  <p><a href="#features">Case studies</a></p>
+  <p><a href="#blog">Library</a></p>
+  </>
+)
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className='gpt4_navbar'>
       <div className='gpt4_navbar-links'>
@@ -11,15 +23,25 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className='gpt4_navbar-links_container'>
-          <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt4">What is GPT?</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case studies</a></p>
-          <p><a href="#blog">Library</a></p>
+          <Menu/>
         </div>
         <div className='gpt4_navbar-sign'>
           <p>Sign in</p>
           <button type='button'>Sign in</button>
+        </div>
+        <div className='gpt4_navbar-menu'>
+          {toggleMenu
+            ? <RiCloseLine color='#fff' size={27} onClick={()=> setToggleMenu(false)}/>
+            : <RiMenu3Line color='#fff' size={27} onClick={()=> setToggleMenu(true)}/>
+          }
+          {toggleMenu && (
+            <div className='gpt4_navbar-menu_container scale-up-center'>
+              <div className='gpt4_navbar-menu_container-links'>
+                <Menu/> 
+              </div>                
+            </div>
+          )
+          }
         </div>
       </div>
     </div>
