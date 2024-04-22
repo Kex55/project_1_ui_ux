396e7bf0ec7d8cdd09a5572742c07887a5c6d359
viktorherman55 <viktorherman55@gmail.com>
2024-04-22 16:29:28 +0200

    Navbar links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,6 +10,13 @@ const Navbar = () => {
         <div className='gpt4_navbar-links_logo'>
           <img src={logo} alt="logo" />
         </div>
+        <div>
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt4">What is GPT?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
       </div>
     </div>
   )
