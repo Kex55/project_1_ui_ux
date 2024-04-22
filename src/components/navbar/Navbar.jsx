e6b7438df27a7b14815237d964e883d7d8b4a27a
viktorherman55 <viktorherman55@gmail.com>
2024-04-22 16:48:25 +0200

    links css, sign in button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,12 +10,16 @@ const Navbar = () => {
         <div className='gpt4_navbar-links_logo'>
           <img src={logo} alt="logo" />
         </div>
-        <div>
+        <div className='gpt4_navbar-links_container'>
           <p><a href="#home">Home</a></p>
           <p><a href="#wgpt4">What is GPT?</a></p>
           <p><a href="#possibility">Open AI</a></p>
           <p><a href="#features">Case studies</a></p>
           <p><a href="#blog">Library</a></p>
+        </div>
+        <div className='gpt4_navbar-sign'>
+          <p>Sign in</p>
+          <button type='button'>Sign in</button>
         </div>
       </div>
     </div>
