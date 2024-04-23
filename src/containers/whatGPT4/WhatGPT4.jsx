cba314928139ecb2d58884ac008c56a33a35a168
viktorherman55 <viktorherman55@gmail.com>
2024-04-23 17:13:26 +0200

    whatgpt4 content

--- src/containers/whatGPT4/WhatGPT4.jsx
+++ src/containers/whatGPT4/WhatGPT4.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './whatGPT4.css';
+import { Feature } from '../../components'
 
 const WhatGPT4 = () => {
   return (
-    <div>WhatGPT4</div>
+    <div className='gpt4_whatgpt4 section_margin' id='whpt4'>
+      <div className='gpt4_whatgpt4-feature'>
+        <Feature/>
+      </div>
+      <div className='gpt4_whatgpt4-heading'>
+        <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='gpt4_whatgpt4-container'>
+        <Feature/>
+        <Feature/>
+        <Feature/>
+      </div>
+    </div>
   )
 }
 
