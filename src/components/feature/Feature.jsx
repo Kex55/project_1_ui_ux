a00ceb821d3ec0cf3df218935e295f98fc7c38ad
viktorherman55 <viktorherman55@gmail.com>
2024-04-23 18:16:31 +0200

    Feature content

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './feature.css';
 
-const Feature = () => {
+const Feature = ({title, text}) => {
   return (
-    <div className='gpt4_features-container_feature'></div>
+    <div className='gpt4_features-container_feature'>
+      <div className='gpt4_features-container_feature-title'>
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className='gpt4_features-container_feature-text'>
+        {text}
+      </div>
+    </div>
   )
 }
 
