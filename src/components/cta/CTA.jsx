b9a1b11bc62f77fbf05d260abe9ccbc95404ea66
viktorherman55 <viktorherman55@gmail.com>
2024-04-24 19:10:05 +0200

    CTA content

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,15 @@ import './cta.css';
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='gp4_cta'>
+      <div className='gp4_cta-content'>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className='gp4_cta-btn'>
+        <button type="button">Get Started</button>
+      </div>
+    </div>
   )
 }
 
