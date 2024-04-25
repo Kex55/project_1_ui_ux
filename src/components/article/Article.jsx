d0248863ad622dcd6b7564ec44a55b912cc1bb90
viktorherman55 <viktorherman55@gmail.com>
2024-04-25 16:11:29 +0200

    Blog image imports

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './article.css';
 
-const Article = () => {
+const Article = ({imgUrl}) => {
   return (
-    <div>Article</div>
+    <div className='gpt4_blog-container_article'>
+      <div className='gpt4_blog-container_article-image'>
+        <img src={imgUrl} alt="blog image" />
+      </div>
+    </div>
   )
 }
 
