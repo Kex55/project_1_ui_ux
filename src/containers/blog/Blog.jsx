ac62953fc8ca9c75b75f1fb0ec373e3232da48fa
viktorherman55 <viktorherman55@gmail.com>
2024-04-25 15:54:34 +0200

    Blog content class structure

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -3,7 +3,20 @@ import './blog.css';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt4_blog section_padding'>
+      <div className='gpt4_blog-heading'>
+        <h1 className='gradient_text'>A lot is happening, 
+We are blogging about it.</h1>
+      </div>
+      <div className='gpt4_blog-container'>
+        <div className='gpt4_blog-container_groupA'>
+
+        </div>
+        <div className='gpt4_blog-container_groupB'>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
