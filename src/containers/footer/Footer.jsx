d3cc7a37096c0fd9ce8295817f6a3a32cc0ce105
viktorherman55 <viktorherman55@gmail.com>
2024-04-25 17:39:59 +0200

    Footer contents

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
 import './footer.css';
+import gptLogo from '../../assets/logo.svg';
 
 const Footer = () => {
   return (
-    <div className='gpt4_footer'>Footer</div>
+    <div className='gpt4_footer section_padding'>
+      <div className='gpt4_footer-heading'>
+        <h1 className='gradient_text'>Do you want to step in to the future before others</h1>
+      </div>
+
+      <div className='gpt4_footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+
+      <div className='gpt4_footer-links'>
+        <div className='gpt4_footer-links_logo'>
+          <img src={gptLogo} alt="logo" />
+          <p>Stenchwoord K55 134 DK BadadaBing, All Rights Reserved</p>
+
+          <div className='gpt4_footer-links_div'>
+            <h4>Links</h4>
+            <p>Overons</p>
+            <p>Social Media</p>
+            <p>Counters</p>
+            <p>Contact</p>
+          </div>
+          <div className='gpt4_footer-links_div'>
+            <h4>Company</h4>
+            <p>Terms & Conditions</p>
+            <p>Privacy Policy</p>
+            <p>Contact</p>
+          </div>
+          <div className='gpt4_footer-links_div'>
+            <h4>Get in touch</h4>
+            <p>Stenchwoord K55 134 DK BadadaBing</p>
+            <p>021-341246</p>
+            <p>info@payme.net</p>
+          </div>
+
+          <div className='gpt4_footer-copyright'>
+            <p>© 2024 GPT-4. All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
